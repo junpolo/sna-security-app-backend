f8e98e49c3ba62ce8346c6b00335c78d2982e6e9
Jun Polo <junpolojr@gmail.com>
2023-08-22 11:03:59 +0800

    feat(deps): add body-parser dependency

--- server.ts
+++ server.ts
@@ -1,11 +1,12 @@
 import * as express from "express";
-import * as cors from "cors";
-
 import { Express, Router, Request, Response } from "express";
+import * as bodyParser from "body-parser";
 
 import { getUsers, getUserById } from "./app/user/controller";
 
 const app: Express = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 const port = 8080;
 
